dac121d34d028210619b77add9dc915f666e8969
Cristhiam D. López Nuñez <cristhiam.lopez.nunez@gmail.com>
2022-12-14 20:58:25 -0500

    User input testing

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import sqlite3
 
-connection = sqlite3.connect("taxi.db")
+# connection = sqlite3.connect("taxi.db")
 
-cursor = connection.cursor()
+# cursor = connection.cursor()
 
-
+var = input("Please enter something: ")
+print("You entered: " + var)
