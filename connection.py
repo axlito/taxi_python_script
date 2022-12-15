ec87f67a20bce80bc2008abcb69f9076012af2ef
Yohan González Almaguer <yohan.gonzalez.almaguer@gmail.com>
2022-12-14 23:08:04 -0500

    show_taxis_with_more_than_200_km function

--- connection.py
+++ connection.py
@@ -1,34 +1,40 @@
 import sqlite3
 import uuid
 
-connection = sqlite3.connect("taxis_db.sqlite")
-
 
 def create_tables():
+    connection = sqlite3.connect("taxis_db.sqlite")
     cursor = connection.cursor()
-    drivers_table = """CREATE TABLE IF NOT EXISTS drivers (
-                        driver_id TEXT PRIMARY KEY UNIQUE,
-                        carnet TEXT NOT NULL,
-                        nombre_completo TEXT NOT NULL UNIQUE,
-                        edad INTEGER NOT NULL
-                    );"""
-    cursor.execute(drivers_table)
-    taxis_table = """CREATE TABLE IF NOT EXISTS taxis (
-                taxi_id TEXT PRIMARY KEY UNIQUE,
-                chapa TEXT NOT NULL,
-                recorrido INTEGER NOT NULL,
-                driver_id TEXT UNIQUE,
-                FOREIGN KEY (driver_id)
-                    REFERENCES drivers (driver_id)
-                    ON DELETE CASCADE
-            );"""
-    cursor.execute(taxis_table)
-    cursor.close()
+    try:
+        drivers_table = """CREATE TABLE IF NOT EXISTS drivers (
+                            driver_id TEXT PRIMARY KEY UNIQUE,
+                            carnet TEXT NOT NULL,
+                            nombre_completo TEXT NOT NULL UNIQUE,
+                            edad INTEGER NOT NULL
+                        );"""
+        cursor.execute(drivers_table)
+        taxis_table = """CREATE TABLE IF NOT EXISTS taxis (
+                    taxi_id TEXT PRIMARY KEY UNIQUE,
+                    chapa TEXT NOT NULL,
+                    recorrido INTEGER NOT NULL,
+                    driver_id TEXT UNIQUE,
+                    FOREIGN KEY (driver_id)
+                        REFERENCES drivers (driver_id)
+                        ON DELETE CASCADE
+                );"""
+        cursor.execute(taxis_table)
+        cursor.close()
+    except sqlite3.Error as error:
+        print(error)
+    finally:
+        if connection:
+            connection.close()
 
 
 def insert_driver(carnet: str, nombre_completo: str, edad: int):
+    connection = sqlite3.connect("taxis_db.sqlite")
+    cursor = connection.cursor()
     try:
-        cursor = connection.cursor()
         sql = ''' INSERT INTO drivers(driver_id,carnet,nombre_completo,edad) VALUES(?,?,?,?); '''
         cursor.execute(sql, (uuid_v4(), carnet, nombre_completo, edad))
         connection.commit()
@@ -42,8 +48,9 @@ def insert_driver(carnet: str, nombre_completo: str, edad: int):
 
 
 def insert_taxi(chapa: str, recorrido: int, nombre_completo: str):
+    connection = sqlite3.connect("taxis_db.sqlite")
+    cursor = connection.cursor()
     try:
-        cursor = connection.cursor()
         cursor.execute('''SELECT driver_id FROM drivers where nombre_completo like ?''', ('%' + nombre_completo + '%',))
         row = cursor.fetchone()
         if row[0]:
@@ -62,12 +69,38 @@ def insert_taxi(chapa: str, recorrido: int, nombre_completo: str):
 
 
 def show_drivers():
+    connection = sqlite3.connect("taxis_db.sqlite")
+    cursor = connection.cursor()
     try:
-        cursor = connection.cursor()
         cursor.execute('''SELECT * FROM drivers''')
         rows = cursor.fetchall()
-        for row in rows:
-            print(f'{row[2]}, DNI: {row[1]}, Edad: {row[3]}\n')
+        if len(rows) > 0:
+            for row in rows:
+                print(f'> {row[2]}, DNI: {row[1]}, Edad: {row[3]}\n')
+        else:
+            print('No drivers stored :(')
+        cursor.close()
+    except sqlite3.Error as error:
+        print(error)
+    finally:
+        if connection:
+            connection.close()
+
+
+def show_taxis_with_more_than_200_km():
+    connection = sqlite3.connect("taxis_db.sqlite")
+    cursor = connection.cursor()
+    try:
+        cursor.execute('''SELECT * FROM taxis 
+                        inner join drivers d on d.driver_id = taxis.driver_id 
+                        where recorrido > 200 
+                        ''')
+        rows = cursor.fetchall()
+        if len(rows) > 0:
+            for row in rows:
+                print(f'> {row[1]}, Recorrido: {row[2]}Km, Chofer: {row[6]}\n')
+        else:
+            print('No taxis stored with more than 200km :(')
         cursor.close()
     except sqlite3.Error as error:
         print(error)
--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-from connection import create_tables, insert_driver, insert_taxi, show_drivers
+from connection import create_tables, insert_driver, insert_taxi, show_drivers, show_taxis_with_more_than_200_km
 
 if __name__ == '__main__':
     create_tables()
-    # insert_driver('97100908262', 'Yohan Gonzalez Almaguer', 25)
-    # insert_taxi('P587421', 10, 'Yohan')
+    # insert_driver('97122558711', 'Cristhiam Lopez', 25)
+    # insert_taxi('P987456', 250, 'Cristhiam')
     # show_drivers()
+    # show_taxis_with_more_than_200_km()
